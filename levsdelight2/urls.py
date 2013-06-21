b3a0c5d5fe87fdddbef5da2f90683b6366fa200c
Jason Levinsohn <jlevinsohn@endgames.us>
2013-06-21 12:29:18 -0400

    added static serving path to urls.py for Heroku

--- levsdelight2/urls.py
+++ levsdelight2/urls.py
@@ -14,4 +14,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    # Needed for Heroku Deployment of Static Files
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    
 )
