1816cfe8b07b1671cc3201719410a948e6600709
Jason Levinsohn <jlevinsohn@endgames.us>
2013-12-05 17:15:20 -0500

    testing import script

---
+++ importSlides.py
@@ -0,0 +1,27 @@
+from levsdelight_com.models import Slideshow
+import csv
+import datetime
+
+with open('/Users/jlevinsohn/Downloads/sllideshow-demo.csv', 'rb') as csvfile:
+    reader = csv.reader(csvfile, delimiter=';', quotechar='"')
+    for row in reader:
+        print row[1]
+        print row[2]
+        print row[3]
+        print row[4]
+        print row[5]
+        print row[6]
+        slide = Slideshow()
+        slide.title = str(row[1])
+        slide.desc = str(row[2])
+        slide.pictureLocation = str(row[3])
+        slide.isActive = int(row[4])
+        slide.slideshow_id = int(row[5])
+        slide.order_id = int(row[6])
+        slide.pub_date = datetime.datetime.now()
+        slide.save()
+# f = open('/Users/jlevinsohn/Downloads/sllideshow-demo.csv', 'r')
+# for line in f:
+#     print line
+
+# f.close()
