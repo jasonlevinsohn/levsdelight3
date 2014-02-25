6bf76187f49cb77fbf7338bb544f47474d439951
Jason Levinsohn <jlevinsohn@endgames.us>
2014-02-25 17:17:19 -0500

    built 2013 slides and added project links

--- importSlides.py
+++ importSlides.py
@@ -2,7 +2,7 @@ from levsdelight_com.models import Slideshow
 import csv
 import datetime
 
-with open('june-id-fix.csv', 'rb') as csvfile:
+with open('2013-slides.csv', 'rb') as csvfile:
     reader = csv.reader(csvfile, delimiter=';', quotechar='"')
     for row in reader:
         print row[1]
