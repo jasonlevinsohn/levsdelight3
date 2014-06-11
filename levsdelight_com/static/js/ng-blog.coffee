720a2615ae03232f235a3070aa048e08db0a2af4
Jason Levinsohn <jason.levinsohn@gmail.com>
2014-06-11 10:35:18 -0400

    added coffee to blog section

--- levsdelight_com/static/js/ng-blog.coffee
+++ levsdelight_com/static/js/ng-blog.coffee
@@ -1,1 +1,28 @@
 blogApp = angular.module 'levs-blog', []
+
+blogApp.controller('BlogCtrl', ['$scope', '$http', ($scope, $http) ->
+    posts = []
+
+    $http.get('/getTopBlogs')
+        .success (data, status) ->
+            console.log data
+            $scope.posts = data
+        .error (data, status) ->
+            console.log data
+            console.log status
+])
+
+
+blogApp.controller('BlogPreviewCtrl', ['$scope', '$http', ($scope, $http) ->
+    posts = []
+
+    $http.get('/getTopBlogs')
+        .success (data, status) ->
+            console.log data
+            $scope.posts = data
+        .error (data, status) ->
+            console.log data
+            console.log status
+])
+
+
--- levsdelight_com/static/js/ng-blog.coffee.js
+++ levsdelight_com/static/js/ng-blog.coffee.js
@@ -3,4 +3,32 @@
 
   blogApp = angular.module('levs-blog', []);
 
+  blogApp.controller('BlogCtrl', [
+    '$scope', '$http', function($scope, $http) {
+      var posts;
+      posts = [];
+      return $http.get('/getTopBlogs').success(function(data, status) {
+        console.log(data);
+        return $scope.posts = data;
+      }).error(function(data, status) {
+        console.log(data);
+        return console.log(status);
+      });
+    }
+  ]);
+
+  blogApp.controller('BlogPreviewCtrl', [
+    '$scope', '$http', function($scope, $http) {
+      var posts;
+      posts = [];
+      return $http.get('/getTopBlogs').success(function(data, status) {
+        console.log(data);
+        return $scope.posts = data;
+      }).error(function(data, status) {
+        console.log(data);
+        return console.log(status);
+      });
+    }
+  ]);
+
 }).call(this);
